5edd89a4c6755cbb7c8c586516f6afdca1beecb2
Cl0st3r <JaekelJulian@dv-schulen.de>
2018-06-11 12:45:16 +0200

    Änderungen in MainWindow.xaml

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,34 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonChoose_Click(object sender, RoutedEventArgs e)
+        {
+            string path;
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                path = file.FileName;
+            }
+        }
+
+        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Passende Lösung für das Problem finden -> Word per Befehl öffnen
+            
+            //Code soweit unbrauchbar.
+            // Load DOCX or DOC document.
+            var document = DocumentModel.Load(isDocx ? "Document.docx" : "Document.doc");
+
+            // Iterate over all paragraphs in the document.
+            foreach (Paragraph para in document.GetChildElements(true, ElementType.Paragraph))
+            {
+                // Iterate over all runs in the paragraph and write their text to Console.
+                foreach (Run run in para.GetChildElements(true, ElementType.Run))
+                    Console.Write(run.Text);
+                Console.WriteLine();
+            }
         }
     }
 }
