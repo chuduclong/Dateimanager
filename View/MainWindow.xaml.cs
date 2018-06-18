e10d6601502b35d698a612dc9e6767db47dbddab
JaekelJulian <JaekelJulian@dv-schulen.de>
2018-06-18 12:40:23 +0200

    Work @ GUI && Work @ FileChooser!

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -26,9 +26,8 @@ namespace View
     {
         private RunFile run1 = new RunFile();
         Datenbank db = null;
-        Boolean word = false;
-        Boolean powerpoint = false;
-        Boolean excel = false;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
         public MainWindow()
         {
             db = new Datenbank();
@@ -42,9 +41,16 @@ namespace View
             
         }
 
-        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        private void buttonHinzufuegen_Click(object sender, RoutedEventArgs e)
         {
-            
+            //TODO: FILEFILTER Bearbeiten, Documente aus Filter in DB übertragen -> Long DB!
+            openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Geeignete Datei auswählen";
+            openFileDialog1.Filter = "Word Documents (*.DOC;*.DOCX)|*.DOC;*.DOCX|";
+            openFileDialog1.Filter = "Excel Workbook (*.XLS;*.XSLX)|*.XLS;*.XLSX|";
+            openFileDialog1.Filter = "PowerPoint Präsentation (*.PPT;*.PPTX)|*.PPT;*.PPTX";
+            openFileDialog1.ShowDialog();
         }
 
         private void buttonDelete_Click(object sender, RoutedEventArgs e)
